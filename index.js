dec1ec001fe5cb3776d071e2acd26298296f7013
shehan013 <shehanmaleesha13@gmail.com>
2024-05-19 15:34:34 +0530

    CRUD_initiate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.use(express.json());    //This need to be above any routes that will use req.body
 
 require('dotenv').config();
 const Project = require('./Project');
@@ -28,6 +29,71 @@ app.get('/blogs', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+
+//create an endpoint for creating a new project
+app.post('/projects', async (req, res) => {
+    
+    console.log(req.body);  //This will print the request body in the console   
+    //res.send('Received the request, Creating a porject');
+    
+    const project = new Project({
+        name: req.body.name,
+        description: req.body.description,
+        
+    });
+
+    try {
+        const newProject = await project.save();
+        res.status(201).json(newProject);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+//Give a sample requset JSON for creating a project
+/*project = {
+    "name": "AI research project",
+    "description": "This is a AI and ML project"
+};*/
+
+//create a end point for updataing project by id using PATCH method
+app.patch('/projects/:id', async (req, res) => {
+    //console.log(req.params.id);  //This will print the request body in the console  
+    try {
+        const project = await Project.findById(req.params.id);
+        if (req.body.name) {
+            project.name = req.body.name;
+        }
+        if (req.body.description) {
+            project.description = req.body.description;
+        }
+        const updatedProject = await project.save();
+        res.json(updatedProject);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+  
+});
+
+//Delete a project by id
+
+app.delete('/projects/:id', async (req, res) => {
+    try {
+        const deletedProject = await Project.findByIdAndDelete(req.params.id);
+        if (!deletedProject) {
+            return res.status(404).json({ message: 'Project not found' });
+        }
+        res.json({ message: 'Project deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
